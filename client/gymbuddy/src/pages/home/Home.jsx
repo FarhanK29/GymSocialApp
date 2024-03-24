b8c4bb5956884351c90ce6f9fc6a956e6041d167
Farhan Khan <farhan.k0429@gmail.com>
2024-03-24 09:23:51 -0400

    login signup updates

--- client/gymbuddy/src/pages/home/Home.jsx
+++ client/gymbuddy/src/pages/home/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 
 export default function Home() {
-  return (
-    <Navbar />
-
+  return(
+    <div>
+        <Navbar />
+        <div className = "button-container">
+            <button className = "post-button">
+                Look for a Buddy!
+            </button>
+        </div>
+    </div>
   )
 }
